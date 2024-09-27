97c83161a878e6aba6d485f4457bddf3a9023de8
Marley <Marley@ENROXES>
2024-09-27 21:07:08 +0500

    Добавьте файлы проекта.

---
+++ pr2_c++.cpp
@@ -0,0 +1,34 @@
+﻿using namespace std;
+
+#include <iostream>
+
+int main()
+{
+	setlocale(0, "rus");
+	int M[6];
+	int i;
+	srand(time(0));
+	for (i = 0; i < 6; i++)
+	{
+		M[i] = 1 + rand() % 20;
+	}
+	for (i = 0; i < 6; i++)
+	{
+		cout << M[i] << " ";
+	}
+	int min;
+	min = M[0];
+	for (i = 1; i < 6; i++)
+	{
+		if (min > M[i])
+		{
+			min = M[i];
+		}
+		else
+		{
+			min = min;
+		}
+	}
+	cout << "\nМинимальное значение массива: " << min;
+}
+
